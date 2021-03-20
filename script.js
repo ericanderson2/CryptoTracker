58eb357ffcbf750b0f2f1c9ca28ea97a5ae2a43d
eanderson <717eanderson@gmail.com>
2021-03-19 22:47:21 -0400

    live price test

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-
 var coinDict = {};
+
+setInterval(getSimplePrice, 1000, "bitcoin")
+
 function getCoins()
 {
     const listURL='https://api.coingecko.com/api/v3/coins/list';
     var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() 
+    xmlhttp.onreadystatechange = function()
     {
         var xmlhttp = new XMLHttpRequest();
-        if (this.readyState == 4 && this.status == 200) 
+        if (this.readyState == 4 && this.status == 200)
         {
             var coinData = JSON.parse(this.responseText);
             for (var i = 0; i < coinData.length; i++) {
-                coinDict[coinData[i].id] = coinData[i].name 
+                coinDict[coinData[i].id] = coinData[i].name
             }
             console.log(coinDict);
         }
@@ -22,11 +24,11 @@ function getCoins()
 function getPrice(coin)
 {
     const coinURL = 'https://api.coingecko.com/api/v3/coins/' + coin + '?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false';
-    console.log(coinURL);
+    //console.log(coinURL);
     var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() 
+    xmlhttp.onreadystatechange = function()
     {
-        if (this.readyState == 4 && this.status == 200) 
+        if (this.readyState == 4 && this.status == 200)
         {
             var coinPrice = JSON.parse(this.responseText);
             console.log(coinPrice);
@@ -34,4 +36,23 @@ function getPrice(coin)
     };
     xmlhttp.open("GET", coinURL, true);
     xmlhttp.send();
+}
+
+function getSimplePrice(coin)
+{
+  const coinURL = 'https://api.coingecko.com/api/v3/simple/price?ids=' + coin + '&vs_currencies=usd&include_market_cap=false&include_24hr_vol=false&include_24hr_change=false&include_last_updated_at=true';
+  //console.log(coinURL);
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function()
+  {
+      if (this.readyState == 4 && this.status == 200)
+      {
+          var coinPrice = JSON.parse(this.responseText);
+          console.log(coinPrice[coin]["usd"]);
+          console.log(coinPrice[coin]["last_updated_at"])
+          document.getElementById(coin).innerHTML = coinPrice[coin]["usd"];
+      }
+  };
+  xmlhttp.open("GET", coinURL, true);
+  xmlhttp.send();
 }
